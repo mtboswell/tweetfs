058b7a1b686490a8071b9829b5fd49b713c4e9bd
sreejithemk <sreejithemk@9ce4419a-a7a2-11de-bb10-370f7287a6b7>
2009-09-26 14:03:32 +0000

    - Added support for Direct Messages - User status deletion - Remove a user from following list

--- tweetfs.py
+++ tweetfs.py
@@ -6,7 +6,7 @@ in a twitter user's directory.
 Author:
     Sreejith K <sreejithemk@gmail.com>
     http://semk.in
-    Copyright (c) 2009
+    Copyright (c) 2009, Sreejith K
 
 Licensed under GNU GPL v3 or later. Refer COPYING for more.
 """
@@ -43,7 +43,11 @@ def tweetfs_update(root, api):
         os.mkdir(home_dir)
 
     # Get the user's tweets
-    tweets = api.GetUserTimeline(USERNAME, count=USER_TWEETS)
+    try:
+        tweets = api.GetUserTimeline(USERNAME, count=USER_TWEETS)
+    except Exception:
+        print 'Problem connecting to twitter. Safe to Ignore'
+        tweets = []
 
     # Write the user's tweets into regular files. Filename will be the tweet id
     for tweet in tweets:
@@ -54,14 +58,24 @@ def tweetfs_update(root, api):
             tf.write(tweet.text + '\n')
             tf.close()
 
+    try:
+        friends = api.GetFriends()
+    except Exception:
+        'Problem connecting to Twitter. Safe to Ignore.'
+        friends = []
+
     # Setup user's friend's directories
-    for friend in api.GetFriends():
+    for friend in friends:
         if not os.path.exists(root + '/' + friend._screen_name):
             print 'Creating directory', root + '/' + friend._screen_name
             os.mkdir(root + '/' + friend._screen_name)
             
         # Get friend's tweets
-        tweets = api.GetUserTimeline(friend._screen_name, count=FRIEND_TWEETS)
+        try:
+            tweets = api.GetUserTimeline(friend._screen_name, count=FRIEND_TWEETS)
+        except Exception:
+            print 'Problem connecting to Twitter. Safe to Ignore.'
+            tweets = []
 
         # Write friend's tweets into regular files inside the friend's directory
         for tweet in tweets:
@@ -126,7 +140,17 @@ class TweetFS(LoggingMixIn, Operations):
         return os.link(source, target)
     
     listxattr = None
-    mkdir = os.mkdir
+    
+    def mkdir(self, path, mode):
+        user = os.path.basename(path)
+        print 'Now following', user
+
+        try:
+            self.api.CreateFriendship(user=user)
+            os.mkdir(path, mode)
+        except Exception:
+            print 'Error occured while following', user
+    
     mknod = os.mknod
     open = os.open
         
@@ -147,7 +171,14 @@ class TweetFS(LoggingMixIn, Operations):
     def rename(self, old, new):
         return os.rename(old, self.root + new)
     
-    rmdir = os.rmdir
+    def rmdir(self, path):
+        user = os.path.basename(path)
+        try:
+            print 'Removing %s from friends list' % user
+            self.api.DestroyFriendship(user=user)
+            os.rmdir(path)
+        except Exception:
+            print 'Error removing friendship'
     
     def statfs(self, path):
         stv = os.statvfs(path)
@@ -162,21 +193,42 @@ class TweetFS(LoggingMixIn, Operations):
         with open(path, 'r+') as f:
             f.truncate(length)
     
-    unlink = os.unlink
+    def unlink(self, path):
+        user_or_id = os.path.basename(path)
+
+        if os.path.isdir(path):
+            try:
+                print 'Removing %s from friends list' % user
+                self.api.DestroyFriendship(user=user_or_id)
+                os.unlink(path)
+            except Exception:
+                'Error removing friendship'
+        else:
+            if os.path.dirname(path).split('/')[-1] == USERNAME:
+                try:
+                    self.api.DestroyStatus(id=user_or_id)
+                except Exception:
+                    'Error removing status'
+                os.unlink(path)
+            else:
+                os.unlink(path)
+        
     utimens = os.utime
     
     def write(self, path, data, offset, fh):
         with self.rwlock:
             os.lseek(fh, offset, 0)
-            if os.path.dirname(path).split('/')[-1] == USERNAME:
-                api = twitter.Api(username=USERNAME, password=PASSWD, input_encoding='utf-8')
+            user = os.path.dirname(path).split('/')[-1]
+
+            # Update the user's status
+            if user == USERNAME:
                 try:
-                    status = api.PostUpdate(data)
+                    status = self.api.PostUpdate(data)
                 except UnicodeDecodeError:
                     print 'Your Message cannot be encoded. Perhaps it contains non-ASCII characters'
+            # Post a Direct Message to friend
             else:
-                raise OSError(EACCES, '')
-                return
+                self.api.PostDirectMessage(user=user, text=data)
             return os.write(fh, data)
     
 
